a831d50d60ac056a010da4ac5c9ed49c295af756
Max Wittal <wittal@neobotix.de>
2020-12-23 15:27:08 +0100

    CUDA_Texture refactor + Mono

--- include/automy/basic_cuda/CUDA_Texture.h
+++ include/automy/basic_cuda/CUDA_Texture.h
@@ -10,6 +10,7 @@
 
 #include <automy/basic_cuda/CUDA.h>
 #include <automy/basic_cuda/CUDA_Image8.hpp>
+#include <automy/basic/Image8.hpp>
 
 
 namespace automy {
@@ -17,27 +18,122 @@ namespace basic_cuda {
 
 class CUDA_Texture {
 public:
-	CUDA_Texture(int width, int height)
-		:	width(width), height(height), array(), res_desc(), tex_desc(), object()
+	CUDA_Texture(int width, int height, int depth, int channels, int sizeof_channel)
+		:	width(width), height(height), depth(depth), channels(channels), sizeof_channel(sizeof_channel),
+			array(), res_desc(), tex_desc(), object()
 	{
 	}
-	
+
 	~CUDA_Texture() {
 		cudaDestroyTextureObject(object);
 		cudaFreeArray(array);
 	}
-	
+
 	CUDA_Texture(const CUDA_Texture&) = delete;
 	CUDA_Texture& operator=(const CUDA_Texture&) = delete;
+
+	int stride() const {
+		return width * channels * sizeof_channel;
+	}
+
+	void upload(const CUDA_Image8& image) {
+		check_dimensions(image);
+		cuda_check(cudaMemcpy2DToArray(array, 0, 0, image.data(), stride(), stride(), height, cudaMemcpyDeviceToDevice));
+	}
+
+	void upload_async(const CUDA_Image8& image, cudaStream_t stream = 0) {
+		check_dimensions(image);
+		cuda_check(cudaMemcpy2DToArrayAsync(array, 0, 0, image.data(), stride(), stride(), height, cudaMemcpyDeviceToDevice, stream));
+	}
+
+	void upload(const basic::Image8& image) {
+		check_dimensions(image);
+		cuda_check(cudaMemcpy2DToArray(array, 0, 0, image.data(), stride(), stride(), height, cudaMemcpyHostToDevice));
+	}
+
+	void upload_async(const basic::Image8& image, cudaStream_t stream = 0) {
+		check_dimensions(image);
+		cuda_check(cudaMemcpy2DToArrayAsync(array, 0, 0, image.data(), stride(), stride(), height, cudaMemcpyHostToDevice, stream));
+	}
+
+	CUDA_Texture& operator=(const CUDA_Image8& image) {
+		upload(image);
+		return *this;
+	}
+
+	CUDA_Texture& operator=(const basic::Image8& image) {
+		upload(image);
+		return *this;
+	}
+
+protected:
+	void allocate(	cudaChannelFormatDesc channel_format,
+					cudaTextureAddressMode address_mode,
+					cudaTextureFilterMode filter_mode,
+					cudaTextureReadMode read_mode,
+					bool normalized_coords)
+	{
+		if(depth < 0) {
+			cuda_check(cudaMallocArray(&array, &channel_format, width, height, cudaArrayTextureGather));
+		} else {
+			cudaExtent extend;
+			extend.width = width;
+			extend.height = height;
+			extend.depth = depth;
+			cuda_check(cudaMalloc3DArray(&array, &channel_format, extend, cudaArrayLayered | cudaArrayTextureGather));
+		}
+		res_desc.resType = cudaResourceTypeArray;
+		res_desc.res.array.array = array;
+
+		tex_desc.addressMode[0] = address_mode;
+		tex_desc.addressMode[1] = address_mode;
+		tex_desc.addressMode[2] = address_mode;
+		tex_desc.filterMode = filter_mode;
+		tex_desc.normalizedCoords = normalized_coords ? 1 : 0;
+		tex_desc.readMode = read_mode;
+
+		cuda_check(cudaCreateTextureObject(&object, &res_desc, &tex_desc, 0));
+	}
 	
+private:
+	template<typename T>
+	void check_dimensions(const T& image) const {
+		if(int(image.width()) != width || int(image.height()) != height || int(image.depth()) != channels) {
+			throw std::logic_error("image.width != width || image.height != height || image.depth != channels");
+		}
+	}
+
+public:
 	int width = 0;
 	int height = 0;
+	int depth = -1;
+	int channels = 0;
+	int sizeof_channel = 0;
 	
 	cudaArray_t array;
 	cudaResourceDesc res_desc;
 	cudaTextureDesc tex_desc;
 	cudaTextureObject_t object;
 	
+};
+
+
+class CUDA_TextureMono : public CUDA_Texture {
+public:
+	CUDA_TextureMono(	int width, int height, int depth = -1,
+						cudaTextureAddressMode address_mode = cudaAddressModeClamp,
+						cudaTextureFilterMode filter_mode = cudaFilterModeLinear,
+						cudaTextureReadMode read_mode = cudaReadModeNormalizedFloat,
+						bool normalized_coords = true)
+		:	CUDA_Texture(width, height, depth, 1, 1)
+	{
+		cudaChannelFormatDesc channel_format = {};
+		channel_format.f = cudaChannelFormatKindUnsigned;
+		channel_format.x = 8;
+
+		allocate(channel_format, address_mode, filter_mode, read_mode, normalized_coords);
+	}
+
 };
 
 
@@ -48,7 +144,7 @@ public:
 						cudaTextureFilterMode filter_mode = cudaFilterModeLinear,
 						cudaTextureReadMode read_mode = cudaReadModeNormalizedFloat,
 						bool normalized_coords = true)
-		:	CUDA_Texture(width, height)
+		:	CUDA_Texture(width, height, depth, 4, 1)
 	{
 		cudaChannelFormatDesc channel_format = {};
 		channel_format.f = cudaChannelFormatKindUnsigned;
@@ -57,48 +153,9 @@ public:
 		channel_format.z = 8;
 		channel_format.w = 8;
 		
-		if(depth < 0) {
-			cuda_check(cudaMallocArray(&array, &channel_format, width, height, cudaArrayTextureGather));
-		} else {
-			cudaExtent extend;
-			extend.width = width;
-			extend.height = height;
-			extend.depth = depth;
-			cuda_check(cudaMalloc3DArray(&array, &channel_format, extend, cudaArrayLayered | cudaArrayTextureGather));
-		}
-		
-		res_desc.resType = cudaResourceTypeArray;
-		res_desc.res.array.array = array;
-		
-		tex_desc.addressMode[0] = address_mode;
-		tex_desc.addressMode[1] = address_mode;
-		tex_desc.addressMode[2] = address_mode;
-		tex_desc.filterMode = filter_mode;
-		tex_desc.normalizedCoords = normalized_coords ? 1 : 0;
-		tex_desc.readMode = read_mode;
-		
-		cuda_check(cudaCreateTextureObject(&object, &res_desc, &tex_desc, 0));
+		allocate(channel_format, address_mode, filter_mode, read_mode, normalized_coords);
 	}
-	
-	void upload(const CUDA_Image8& image) {
-		if(image.width() != width || image.height() != height || image.depth() != 4) {
-			throw std::logic_error("image.width != width || image.height != height || image.depth != 4");
-		}
-		cuda_check(cudaMemcpy2DToArray(array, 0, 0, image.get_data(), width * 4, width * 4, height, cudaMemcpyDeviceToDevice));
-	}
-	
-	void upload_async(const CUDA_Image8& image) {
-		if(image.width() != width || image.height() != height || image.depth() != 4) {
-			throw std::logic_error("image.width != width || image.height != height || image.depth != 4");
-		}
-		cuda_check(cudaMemcpy2DToArrayAsync(array, 0, 0, image.get_data(), width * 4, width * 4, height, cudaMemcpyDeviceToDevice));
-	}
-	
-	CUDA_TextureRGBA& operator=(const CUDA_Image8& image) {
-		upload(image);
-		return *this;
-	}
-	
+
 };
 
 
